9a07a2cfb3ae0743b30b26879aaabcb72b6d9281
Ananda Muhammad Muthaqin <anandamuhammadmtq@gmail.com>
2020-12-08 10:14:23 +0800

    New user signup using JWT&cookie-parser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 // Handle errors
 const handleErrors = (err) => {
@@ -20,6 +21,13 @@ const handleErrors = (err) => {
   return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60; // 3 days in seconds
+const createToken = (id) => {
+  return jwt.sign({ id }, 'secure password', {
+    expiresIn: maxAge
+  });
+}
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 }
@@ -33,7 +41,9 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwtCookies', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   } catch (error) {
     const errors = handleErrors(error);
     res.status(400).json({errors});
